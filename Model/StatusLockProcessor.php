c3b6853152d1fe5d3fd8ecc0fafdc69899aab7b7
theexten <serhiy@softcommerce.co.uk>
2023-02-12 10:08:30 +0000

    - **Enhancement**: Added process validation chain to allow simpler profile process interception.

---
+++ Model/StatusLockProcessor.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Copyright © Soft Commerce Ltd. All rights reserved.
+ * See LICENSE.txt for license details.
+ */
+
+declare(strict_types=1);
+
+namespace SoftCommerce\ProfileSchedule\Model;
+
+/**
+ * @inheritDoc
+ */
+class StatusLockProcessor
+{
+    //@todo: undo locked orders
+}
